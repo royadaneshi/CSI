9e1648f427e1ddc635f3acd1a4262f4344367268
mojtaba nafez <mojtabanafez96@gmail.com>
2023-10-22 00:48:41 +0330

    update

--- train.py
+++ train.py
@@ -67,7 +67,8 @@ for epoch in range(start_epoch, P.epochs + 1):
             "--load_path", str(P.load_path),
             "--normal_labels", str(P.normal_labels),
             "--noise_scale",str(0.0),
-            "--noist_probability", str(0.0)
+            "--noist_probability", str(0.0),
+            '--activation_function', str(P.activation_function)
         ]
 
         result = subprocess.run(["python", "eval.py"] + arguments_to_pass, capture_output=True, text=True)
