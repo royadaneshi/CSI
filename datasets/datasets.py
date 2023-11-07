211a7a9c149a9a61cc433f1f8c0efbc52a570427
mojtaba nafez <mojtabanafez96@gmail.com>
2023-11-07 13:15:14 +0330

    update

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -1164,7 +1164,7 @@ def get_dataset(P, dataset, test_only=False, image_size=(32, 32, 3), download=Fa
         print("test_set shapes: ", test_set[0][0].shape)
         print("len(test_set), len(train_set): ", len(test_set), len(train_set))
     elif dataset == 'cifar100-versus-other-eval':
-        n_classes = 20
+        n_classes = 2
         cifar_transform = transforms.Compose([
                 transforms.Resize((32, 32)),
                 transforms.ToTensor(),
