b6ba2410510fe62d009a76c8f2b7bc9401acb6ee
mojtaba nafez <mojtabanafez96@gmail.com>
2024-02-11 01:46:55 +0330

    add R50ViT model

--- models/vit.py
+++ models/vit.py
@@ -71,9 +71,40 @@ class Pretrain_DINO(BaseModel):
         z_n = F.normalize(z1, dim=-1)
         return z_n
 
+
+class Pretrain_R50_VIT(BaseModel):
+    def __init__(self, num_classes=10, freezing_layer=290):
+        last_dim = 768
+        super(Pretrain_R50_VIT, self).__init__(last_dim, num_classes)
+
+        self.in_planes = 64
+        self.last_dim = 768
+
+        mu = torch.tensor([0.5, 0.5, 0.5]).view(3, 1, 1).cuda()
+        std = torch.tensor([0.5, 0.5, 0.5]).view(3, 1, 1).cuda()
+        self.norm = lambda x: ( x - mu ) / std
+        self.backbone = timm.create_model("vit_base_resnet50_224_in21k")
+        i = 0
+        num = freezing_layer
+        for param in self.backbone.parameters():
+            if i<num:
+                param.requires_grad = False
+            i+=1
+      
+    def penultimate(self, x, all_features=False):
+        x = self.norm(x)
+        z1 = self.backbone(x)
+        z_n = F.normalize(z1, dim=-1)
+        return z_n
+
+
 def VIT_Pretrain(num_classes, freezing_layer=133):
     return Pretrain_VIT(num_classes=num_classes, freezing_layer=freezing_layer)
 
 
 def DINO_Pretrain(num_classes, freezing_layer=133):
     return Pretrain_DINO(num_classes=num_classes, freezing_layer=freezing_layer)
+
+
+def R50_VIT_Pretrain(num_classes, freezing_layer=290):
+    return Pretrain_R50_VIT(num_classes=num_classes, freezing_layer=freezing_layer)
