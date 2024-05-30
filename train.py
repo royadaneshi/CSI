a14b09c09c6e5b0e0c5612d3375e399cd92085d5
your_username <royadaneshi2001@gmail.com>
2024-05-30 10:58:57 +0330

    debugg

--- train.py
+++ train.py
@@ -41,6 +41,7 @@ for epoch in range(start_epoch, P.epochs + 1):
     kwargs['linear_optim'] = linear_optim
     kwargs['simclr_aug'] = simclr_aug
 
+
     if epoch > P.unfreeze_pretrain_model_epoch:
         for param in model.parameters():
             param.requires_grad = True
